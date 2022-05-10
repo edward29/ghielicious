e2f7167830ec69cffe1d33e2a7588e9a6b398917
edward29 <caacbayedward@gmail.com>
2022-05-10 14:58:19 +0800

    test

--- main.js
+++ main.js
@@ -31,15 +31,69 @@ for (let i = 0; i < menuBtn.length; i++) {
 // start of recipe script
 const detailsCon = document.querySelector(".details-container");
 const recipeBtn = Array.from(document.querySelectorAll('.recipe-btn'));
+const img = document.querySelector("#detailsImg");
+const instruction = document.querySelector("#instruction");
+const ingredients = document.querySelector("#ingredients");
 recipeBtn.map( button => {
     button.addEventListener('click', (e) => {
         console.log(e.target.id);
         switch (e.target.id) {
             case 'viewRecipe1':
                 detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 1";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
                 console.log('1');
                 break;
-        
+            case 'viewRecipe2':
+                detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 2";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
+                console.log('2');
+                break;
+            case 'viewRecipe3':
+                detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 3";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
+                console.log('3');
+                break;
+            case 'viewRecipe4':
+                detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 4";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
+                console.log('4');
+                break;
+            case 'viewRecipe5':
+                detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 5";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
+                console.log('5');
+                break;
+            case 'viewRecipe6':
+                detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 6";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
+                console.log('6');
+                break;
+            case 'viewRecipe7':
+                detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 7";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
+                console.log('7');
+                break;
+            case 'viewRecipe8':
+                detailsCon.style.display = "block";
+                img.src = "img/cake1.png";
+                instruction.innerText = "sample 8";
+                ingredients.innerHTML = "dfsgsg<br/>sdgsgs <br/>dsdgsgfs<br/>sdgsg";
+                console.log('8');
+                break;
             default:
                 console.log('error');
                 break;
